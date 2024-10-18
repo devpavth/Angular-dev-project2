54254da1bd8b4898a4a4c02bc36b7724ab83b85b
pavi <paviguna116@gmail.com>
2024-10-18 22:34:52 +0530

    Updated module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,6 +6,10 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { CourseContentComponent } from '../components/course-content/course-content.component';
+import { TabsContentComponent } from '../components/tabs-content/tabs-content.component';
+import { ChatBoxComponent } from '../components/chat-box/chat-box.component';
+
 
 @NgModule({
   imports: [
@@ -14,6 +18,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, CourseContentComponent, TabsContentComponent, ChatBoxComponent]
 })
 export class HomePageModule {}
